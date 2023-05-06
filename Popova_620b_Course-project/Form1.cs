c1c206c593597297de5d158938916e4c5fcf4a7c
Анна Попова <ohgthghr@gmail.com>
2023-05-06 13:36:49 +0300

    Finish Task number 2

--- Popova_620b_Course-project/Form1.cs
+++ Popova_620b_Course-project/Form1.cs
@@ -13,6 +13,7 @@ namespace Popova_620b_Course_project
     public partial class Form1 : Form
     {
         private bool Mode;
+        private MajorWork MajorObject;
         public Form1()
         {
             InitializeComponent();
@@ -27,6 +28,10 @@ namespace Popova_620b_Course_project
 
         private void Form1_Load(object sender, EventArgs e)
         {
+            About A = new About(); // створення форми About
+            A.tAbout.Start();
+            A.ShowDialog(); // відображення діалогового вікна About
+            MajorObject = new MajorWork();
             this.Mode = true;
         }
 
@@ -46,7 +51,11 @@ namespace Popova_620b_Course_project
                 tClock.Stop();
                 bStart.Text = "Пуск";// зміна тексту на кнопці на "Пуск"
                 this.Mode = true;
+                MajorObject.Write(tbInput.Text);// Запис даних у об'єкт
+                MajorObject.Task();// Обробка даних
+                label1.Text = MajorObject.Read();// Відображення результату
             }
+
         }
 
         private void tbInput_KeyPress(object sender, KeyPressEventArgs e)
